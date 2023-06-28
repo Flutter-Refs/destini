643a465f9c0c040f6f1b8abbc2d475891e3c6ddf
yyankov <yavor.yankov@lom.com>
2023-06-28 12:55:42 +0100

    Show/hide second option btn

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -35,6 +35,8 @@ class StoryBrain {
   ];
   int _storyNumber = 0;
 
+  bool get showHideSecondOptionBtn => (_storyNumber == 0 || _storyNumber == 1 || _storyNumber == 2) ? true : false;
+
   String getStory() => _storyData[_storyNumber].title;
 
   String getFirstChoice() => _storyData[_storyNumber].firstChoice;
@@ -76,8 +78,7 @@ class StoryBrain {
     }
   }
 
-  void restart() => _storyNumber = 0;
+  void restart() {
+    _storyNumber = 0;
+  }
 }
-
-
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
